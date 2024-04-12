a5df79d934b38b9e78a9e77ac31281397f352eeb
xiuxiubiu <xiuxiubiu@gmail.com>
2024-04-12 10:31:16 +0800

    upgrade: bufferline configuration

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -53,9 +53,17 @@ require('bufferline').setup {
         end,
         -- offsets = {{filetype = "NvimTree", text = "File Explorer" | function , text_align = "left" | "center" | "right"}},
         color_icons = true, -- true | false, -- whether or not to add the filetype icon highlights
+        get_element_icon = function(element)
+            -- element consists of {filetype: string, path: string, extension: string, directory: string}
+            -- This can be used to change how bufferline fetches the icon
+            -- for an element e.g. a buffer or a tab.
+            -- e.g.
+            local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(
+                                 element.filetype, {default = true})
+            return icon, hl
+        end,
         show_buffer_icons = true, -- true | false, -- disable filetype icons for buffers
         show_buffer_close_icons = true, -- true | false,
-        show_buffer_default_icon = true, -- true | false, -- whether or not an unrecognised filetype should show a default icon
         show_close_icon = true, -- true | false,
         show_tab_indicators = false, -- true | false,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
