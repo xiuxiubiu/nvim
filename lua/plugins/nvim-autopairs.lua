cfabf5d2daad226715d8e4386b66e2a87d229c64
xiuxiubiu <xiuxiubiu@gmail.com>
2026-04-24 15:43:02 +0800

    feat: integrate nvim-autopairs with nvim-cmp for function completions
    Wire nvim-autopairs.completion.cmp to cmp.event confirm_done so that
    selecting a function or method from the completion menu automatically
    inserts () and places the cursor inside.

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -2,6 +2,12 @@ return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	lazy = false,
-	config = true,
-	opts = {},
+	config = function()
+		local npairs = require("nvim-autopairs")
+		npairs.setup({})
+		-- Integrate with nvim-cmp to add () on function completion
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp = require("cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
